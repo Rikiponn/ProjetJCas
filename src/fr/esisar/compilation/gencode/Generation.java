6e52b268af21ab551e80ef72c7ee3fb3c5dbb50f
Sébastien Andre <sebastien.andre585@gmail.com>
2017-11-09 10:43:51 +0100

    Modif de l'instruction Write + qq morceau pour coder_EXP

--- src/fr/esisar/compilation/gencode/Generation.java
+++ src/fr/esisar/compilation/gencode/Generation.java
@@ -1,5 +1,7 @@
 package fr.esisar.compilation.gencode;
 
+import java.util.ArrayList;
+
 import fr.esisar.compilation.global.src.*;
 import fr.esisar.compilation.global.src3.*;
 
@@ -8,19 +10,24 @@ import fr.esisar.compilation.global.src3.*;
  */
 
 class Generation {
-   
+	public static ArrayList<String> decl;
    /**
     * Méthode principale de génération de code.
     * Génère du code pour l'arbre décoré a.
     */
+	
    static Prog coder(Arbre a) {
       Prog.ajouterGrosComment("Programme généré par JCasc");
-
+      decl = new ArrayList<String>();
+      Inst inst;
       // -----------
       // A COMPLETER
       // -----------
       
       coder_Decl(a.getFils1());
+      //Réserve de la place pour les variables locales
+      inst = Inst.creation1(Operation.ADDSP, Operande.creationOpEntier(decl.size()));
+      
       coder_Inst(a.getFils2());
       // L'instruction "new_line"
       // L'instruction "write"
@@ -28,16 +35,29 @@ class Generation {
 
       // Fin du programme
       // L'instruction "HALT"
-      Inst inst = Inst.creation0(Operation.HALT);
+      inst = Inst.creation1(Operation.SUBSP, Operande.creationOpEntier(decl.size()));
+      inst = Inst.creation0(Operation.HALT);
       // On ajoute l'instruction à la fin du programme
       Prog.ajouter(inst);
 
       // On retourne le programme assembleur généré
       return Prog.instance(); 
    }
+   // Remplis une ArrayList qui contient la liste des variables
    private static void coder_Decl(Arbre a){
-   
+	   switch(a.getNoeud()){
+   		case Vide:
+   			break;
+   		case ListeDecl:
+   			coder_Decl(a.getFils1());
+			coder_Decl(a.getFils2());
+   			break;
+   		case ListeIdent:
+   			decl.add(a.getFils2().getChaine());
+			coder_Decl(a.getFils1());
+	   }
    }
+   
    
    private static void coder_Inst(Arbre a){
 	   switch(a.getNoeud()){
@@ -69,30 +89,80 @@ class Generation {
 		   coder_Ecriture(f1.getFils2());
 	   }
 	   //Sinon, celà veut dire qu'on est le plus profond possible, et qu'on ne se servira pas du fils 1 (car il est Vide)
-	   
-	   //Si on veut écrire un string
-	   if(f2.getDecor().getType().equals(Type.String)){
-		   Inst inst = Inst.creation1(Operation.WSTR,Operande.creationOpChaine(f2.getChaine()));
-		   Prog.ajouter(inst,"Ecriture du string : "+f2.getChaine());
+	   else{
+		   //Si on veut écrire un string
+		   if(f2.getDecor().getType().equals(Type.String)){
+			   Inst inst = Inst.creation1(Operation.WSTR,Operande.creationOpChaine(f2.getChaine()));
+			   Prog.ajouter(inst,"Ecriture du string : "+f2.getChaine());
+		   }
+		   //Si on veut écrire un integer
+		   if(f2.getDecor().getType().equals(Type.Integer)){
+			   Inst inst = Inst.creation1(Operation.WINT,coder_EXP(f2));
+			   Prog.ajouter(inst,"Ecriture de l'integer : " );
+		   }
+		   //Si on veut écrire un real
+		   if(f2.getDecor().getType().equals(Type.Real)){
+			   Inst inst = Inst.creation1(Operation.WFLOAT,coder_EXP(f2));
+			   Prog.ajouter(inst,"Ecriture du reel : " );
+		   }
 	   }
-	   //Si on veut écrire un integer
-	   if(f2.getDecor().getType().equals(Type.Integer)){
-		   Inst inst = Inst.creation1(Operation.WINT,coder_EXP(f2));
-		   Prog.ajouter(inst,"Ecriture de l'integer : " );
-	   }
-	   //Si on veut écrire un real
-	   if(f2.getDecor().getType().equals(Type.Real)){
-		   Inst inst = Inst.creation1(Operation.WFLOAT,coder_EXP(f2));
-		   Prog.ajouter(inst,"Ecriture du reel : " );
-	   }
-	   
    }
    
    private static Operande coder_EXP(Arbre a){
+	   Inst inst;
 	   //chercher en fonction du nom de la variable, sa position en pile,PUIS la charger dans un registre libre
 	   //Il faut donc utiliser la fonction getGrosRegistreLibre()
 	   //Il faut ensuite modifier le tableau regTab pour indiquer les registres qui ne sont plus disponible
 	   //
+	   if(a.getArite()==1){
+		   switch(a.getNoeud()){
+		   	case Non:
+		   		break;
+		   	
+		   	case PlusUnaire:
+		   		break;
+		   	
+		   	case MoinsUnaire:
+		  		break;
+		   }
+	   }
+	   if(a.getArite()==2){
+		   switch(a.getNoeud()){
+		   	case Et :
+		   	case Mult :
+		   		// Il faudrai faire en sorte que si la valeur de a.getFils1() est à faux ou 0, ne pas évaluer le deuxième et juste mettre faux ou à 0 dans un registre 
+		   		inst = Inst.creation2(Operation.MUL, coder_EXP(a.getFils1()), coder_EXP(a.getFils2()));
+		   		break;
+		   		
+	   		case Ou :
+	   		case Plus :
+	   			// Il faudrai faire en sorte que si la valeur de a.getFils1() est à vrai, ne pas évaluer le deuxième et juste mettre vrai (pour le OU) dans un registre 
+	   			inst = Inst.creation2(Operation.ADD, coder_EXP(a.getFils1()), coder_EXP(a.getFils2()));
+		   		break;
+		   		
+	   		case Moins :
+	   			inst = Inst.creation2(Operation.SUB, coder_EXP(a.getFils1()), coder_EXP(a.getFils2()));
+		   		break;
+		   		
+	   		case DivReel :
+	   		case Quotient :
+	   			inst = Inst.creation2(Operation.DIV, coder_EXP(a.getFils1()), coder_EXP(a.getFils2()));
+		   		break;	
+		   		
+	   		case Egal :
+	   		case InfEgal :
+	   		case Inf :
+	   		case SupEgal :
+	   		case Sup :
+	   		case NonEgal :
+	   			inst = Inst.creation2(Operation.CMP, coder_EXP(a.getFils1()), coder_EXP(a.getFils2()));
+		   		break;
+		   }
+	   }
+	   if(a.getArite()==3){
+		   switch(a.getNoeud()){
+		   }
+	   }
 	   
 	   return Operande.R0;
    }
