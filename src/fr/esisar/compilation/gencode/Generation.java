0db0616866e4ee5e530842d862536b20df32e3cb
Timothee <communiste4012@gmail.com>
2017-11-30 21:17:18 +0100

    980.equals

--- src/fr/esisar/compilation/gencode/Generation.java
+++ src/fr/esisar/compilation/gencode/Generation.java
@@ -978,11 +978,11 @@ class Generation {
                 reg1 = GestionRegistre.getFreeRegToOpTab();
                 boolean forcementFaux = false;
                 if(a.getFils1().getNoeud() == Noeud.Ident){
-                    if(a.getFils1().getChaine()=="true"){
+                    if(a.getFils1().getChaine().equals("true")){
                         Inst loadInst = Inst.creation2(Operation.LOAD, Operande.creationOpEntier(1), reg1);
                         Prog.ajouter(loadInst, "Chargement de la variable booleenne true (1) dans le registre " + reg1.getRegistre());
                     }
-                    else if(a.getFils1().getChaine() == "false"){
+                    else if(a.getFils1().getChaine().equals("false")){
                         Inst loadInst = Inst.creation2(Operation.LOAD, Operande.creationOpEntier(-1), reg1);
                         Prog.ajouter(loadInst, "Chargement de la variable booleenne false (-1) dans le registre " + reg1.getRegistre());
                         forcementFaux = true;
